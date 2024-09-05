94d56dc53e2708c586f5514c5f32d2a030fe6944
Prince-Sarfo <snellmaxi6@gmail.com>
2024-09-05 16:08:39 +0000

    feat: installed react using vite and tailwind

---
+++ src/components/home_page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './navbar'
+
+const HomePage = () => {
+  return (
+
+    <>
+    {/* navbar */}
+    <Navbar />
+    {/* hero section */}
+    {/* about section */}
+    {/* services section */}
+    {/* portfolio section */}
+    {/* contact section */}
+    {/* footer section */}
+    </>
+  )
+}
+
+export default HomePage
