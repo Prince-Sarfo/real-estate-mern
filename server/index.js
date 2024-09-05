df94272cb87b1489048c47946d557dae583521d9
Prince-Sarfo <snellmaxi6@gmail.com>
2024-09-05 21:41:32 +0000

    feat: connect server to the mongodb database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((error)=>{
+    console.log(error)
+});
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
+
+
