356d3322ed1a84927502f59d8f097c8446abf76e
Prince-Sarfo <snellmaxi6@gmail.com>
2024-09-05 19:05:09 +0000

    feat: create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 
-import './App.css'
-import HomePage from './components/home_page'
 function App() {
-
   return (
     <>
-      <HomePage />
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
