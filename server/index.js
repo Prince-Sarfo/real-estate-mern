262c52ef7b69780afed9c1acd595dc19784d614a
Prince-Sarfo <snellmaxi6@gmail.com>
2024-09-06 21:04:28 +0000

    feat: create test api

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRouter from "./routes/user.routes.js";
 dotenv.config();
 
 
@@ -16,5 +16,9 @@ const app = express();
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
+
+
+
+app.use('/api/user', userRouter);
 
 
